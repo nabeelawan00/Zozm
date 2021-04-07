f4fd2e42582eeb87e28e3d05fb31742c06993389
Bills Mac <nabeelawan.g@gmail.com>
2021-04-07 23:57:42 +0500

    Butter knife plugin installed. First activity done.

--- app/src/main/java/com/app/zozm/activities/FirstCatagoryItemsActivity.java
+++ app/src/main/java/com/app/zozm/activities/FirstCatagoryItemsActivity.java
@@ -1,16 +1,143 @@
 package com.app.zozm.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.app.zozm.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class FirstCatagoryItemsActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_one)
+    TextView tvOne;
+    @BindView(R.id.tv_two)
+    TextView tvTwo;
+    @BindView(R.id.tv_three)
+    TextView tvThree;
+    @BindView(R.id.tv_four)
+    TextView tvFour;
+    @BindView(R.id.tv_five)
+    TextView tvFive;
+    @BindView(R.id.tv_six)
+    TextView tvSix;
+    @BindView(R.id.tv_seven)
+    TextView tvSeven;
+    @BindView(R.id.tv_eight)
+    TextView tvEight;
+    @BindView(R.id.tv_nine)
+    TextView tvNine;
+    @BindView(R.id.tv_ten)
+    TextView tvTen;
+    @BindView(R.id.tv_eleven)
+    TextView tvEleven;
+    @BindView(R.id.tv_tewelve)
+    TextView tvTewelve;
+    @BindView(R.id.tv_tera)
+    TextView tvTera;
+    @BindView(R.id.tv_choda)
+    TextView tvChoda;
+    @BindView(R.id.tv_pandra)
+    TextView tvPandra;
+    @BindView(R.id.tv_solla)
+    TextView tvSolla;
+    @BindView(R.id.button2)
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_first_catagory_items);
+        ButterKnife.bind(this);
+
+
+    }
+
+    @OnClick({R.id.tv_one, R.id.tv_two, R.id.tv_three, R.id.tv_four, R.id.tv_five, R.id.tv_six, R.id.tv_seven, R.id.tv_eight, R.id.tv_nine, R.id.tv_ten, R.id.tv_eleven, R.id.tv_tewelve, R.id.tv_tera, R.id.tv_choda, R.id.tv_pandra, R.id.tv_solla, R.id.button2})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_one:
+
+                tvOne.getBackground().setColorFilter(getResources().getColor(R.color.colorPrimary),
+                        PorterDuff.Mode.SRC_ATOP);
+
+                break;
+            case R.id.tv_two:
+                tvTwo.getBackground().setColorFilter(getResources().getColor(R.color.colorPrimary),
+                        PorterDuff.Mode.SRC_ATOP);
+                break;
+            case R.id.tv_three:
+                tvThree.getBackground().setColorFilter(getResources().getColor(R.color.colorPrimary),
+                        PorterDuff.Mode.SRC_ATOP);
+                break;
+            case R.id.tv_four:
+                tvFour.getBackground().setColorFilter(getResources().getColor(R.color.colorPrimary),
+                        PorterDuff.Mode.SRC_ATOP);
+                break;
+            case R.id.tv_five:
+                tvFive.getBackground().setColorFilter(getResources().getColor(R.color.colorPrimary),
+                        PorterDuff.Mode.SRC_ATOP);
+                break;
+            case R.id.tv_six:
+                tvSix.getBackground().setColorFilter(getResources().getColor(R.color.colorPrimary),
+                        PorterDuff.Mode.SRC_ATOP);
+                break;
+            case R.id.tv_seven:
+                tvSeven.getBackground().setColorFilter(getResources().getColor(R.color.colorPrimary),
+                        PorterDuff.Mode.SRC_ATOP);
+                break;
+            case R.id.tv_eight:
+                tvEight.getBackground().setColorFilter(getResources().getColor(R.color.colorPrimary),
+                        PorterDuff.Mode.SRC_ATOP);
+                break;
+            case R.id.tv_nine:
+                tvNine.getBackground().setColorFilter(getResources().getColor(R.color.colorPrimary),
+                        PorterDuff.Mode.SRC_ATOP);
+                break;
+            case R.id.tv_ten:
+                tvTen.getBackground().setColorFilter(getResources().getColor(R.color.colorPrimary),
+                        PorterDuff.Mode.SRC_ATOP);
+                break;
+            case R.id.tv_eleven:
+                tvEleven.getBackground().setColorFilter(getResources().getColor(R.color.colorPrimary),
+                        PorterDuff.Mode.SRC_ATOP);
+                break;
+            case R.id.tv_tewelve:
+                tvTewelve.getBackground().setColorFilter(getResources().getColor(R.color.colorPrimary),
+                        PorterDuff.Mode.SRC_ATOP);
+                break;
+            case R.id.tv_tera:
+                tvTera.getBackground().setColorFilter(getResources().getColor(R.color.colorPrimary),
+                        PorterDuff.Mode.SRC_ATOP);
+                break;
+            case R.id.tv_choda:
+                tvChoda.getBackground().setColorFilter(getResources().getColor(R.color.colorPrimary),
+                        PorterDuff.Mode.SRC_ATOP);
+                break;
+            case R.id.tv_pandra:
+                tvPandra.getBackground().setColorFilter(getResources().getColor(R.color.colorPrimary),
+                        PorterDuff.Mode.SRC_ATOP);
+                break;
+            case R.id.tv_solla:
+                tvSolla.getBackground().setColorFilter(getResources().getColor(R.color.colorPrimary),
+                        PorterDuff.Mode.SRC_ATOP);
+                break;
+            case R.id.button2:
+
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+
+                break;
+        }
     }
 }
